b6e5f06720796befd58df5ad2e44dc7c522891bc
Huzaifaumer222 <huzaifaghouri206@gmail.com>
2024-05-29 21:10:55 +0500

    first commit

---
+++ porjectcalculator.ts
@@ -0,0 +1,21 @@
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+function subtract(a: number, b: number): number {
+    return a - b;
+}
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+function divide(a: number, b: number): number {
+    if (b === 0) throw new Error("Division by zero is not allowed.");
+    return a / b;
+}
+
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
+console.log(divide(10, 5));
